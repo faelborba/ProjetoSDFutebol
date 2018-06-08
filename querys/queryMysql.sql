019057ae2d1f1259a756bf0cd7b4896c8e3910a5
Rafael Borba <rafael.bds25@gmail.com>
2018-06-08 18:59:51 -0300

    adicionando arquivo sql da base mysql, para usar basta improtar para um banco mysql

--- querys/queryMysql.sql
+++ querys/queryMysql.sql
@@ -82,23 +82,22 @@ where `Player`.`player_name` LIKE ('%Neymar%')
     and upper(`Team`.`team_long_name`) like upper('%Barcelona%')
     and year(`Match`.`date`) = '2015'
 union all
-SELECT year(`Match`.`date`), `Player`.`player_name`, `Team`.`team_long_name`,(`Match`.`home_team_goal` - `Match`.`away_team_goal`) as resultado
+SELECT year(`Match`.`date`), `Player`.`player_name`, `Team`.`team_long_name`,(`Match`.`away_team_goal` - `Match`.`home_team_goal`) as resultado
 FROM `Player`
 INNER JOIN `Match`
-    on `Match`.`home_player_1` = `Player`.`player_api_id`
-    or `Match`.`home_player_2` = `Player`.`player_api_id`
-    or `Match`.`home_player_3` = `Player`.`player_api_id`
-    or `Match`.`home_player_4` = `Player`.`player_api_id`
-    or `Match`.`home_player_5` = `Player`.`player_api_id`
-    or `Match`.`home_player_6` = `Player`.`player_api_id`
-    or `Match`.`home_player_7` = `Player`.`player_api_id`
-    or `Match`.`home_player_8` = `Player`.`player_api_id`
-    or `Match`.`home_player_9` = `Player`.`player_api_id`
-    or `Match`.`home_player_10` = `Player`.`player_api_id`
-    or `Match`.`home_player_11` = `Player`.`player_api_id`
+    on `Match`.`away_player_1` = `Player`.`player_api_id`
+    or `Match`.`away_player_2` = `Player`.`player_api_id`
+    or `Match`.`away_player_3` = `Player`.`player_api_id`
+    or `Match`.`away_player_4` = `Player`.`player_api_id`
+    or `Match`.`away_player_5` = `Player`.`player_api_id`
+    or `Match`.`away_player_6` = `Player`.`player_api_id`
+    or `Match`.`away_player_7` = `Player`.`player_api_id`
+    or `Match`.`away_player_8` = `Player`.`player_api_id`
+    or `Match`.`away_player_9` = `Player`.`player_api_id`
+    or `Match`.`away_player_10` = `Player`.`player_api_id`
+    or `Match`.`away_player_11` = `Player`.`player_api_id`
 INNER JOIN `Team`
-    on `Match`.`home_team_api_id` = `Team`.`team_api_id`
+    on `Match`.`away_team_api_id` = `Team`.`team_api_id`
 where `Player`.`player_name` LIKE ('%Neymar%')
     and upper(`Team`.`team_long_name`) like upper('%Barcelona%')
     and year(`Match`.`date`) = '2015';
-
