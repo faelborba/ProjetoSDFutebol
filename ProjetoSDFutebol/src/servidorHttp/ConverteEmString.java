d48a925ff4b401a6e4a9bc797b62413ba9a945e6
Rafael Borba <rafael.bds25@gmail.com>
2018-06-10 22:52:15 -0300

    Cração do arquivo config com os dados do servidor, criação da classe configclass e adicionando as entradas desse arquivo no servidor http

---
+++ ProjetoSDFutebol/src/servidorHttp/ConverteEmString.java
@@ -0,0 +1,23 @@
+package servidorHttp;
+
+import java.util.Scanner;
+
+public class ConverteEmString {
+	private String linha = "";
+
+	public String converteJson(Scanner entrada) {
+		while (entrada.hasNextLine()) {
+			linha = linha + entrada.nextLine() + "\n";
+			// System.out.println(entrada.nextLine());
+		}
+		return this.linha;
+	}
+
+	public String getLinha() {
+		return linha;
+	}
+
+	public void setLinha(String linha) {
+		this.linha = linha;
+	}
+}
---
+++ ProjetoSDFutebol/src/servidorHttp/Erro.java
@@ -0,0 +1,23 @@
+package servidorHttp;
+
+public class Erro {
+	public int errorCode = 0;
+	public String errorDescription = null;
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorDescription() {
+		return errorDescription;
+	}
+
+	public void setErrorDescription(String errorDescription) {
+		this.errorDescription = errorDescription;
+	}
+	
+}
